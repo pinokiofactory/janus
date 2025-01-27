615f7e212dbe08bd470ba3410a3eef20bb8cb114
cocktailpeanut <cocktailpeanuts@proton.me>
2025-01-27 18:52:38 -0500

    update

--- install.js
+++ install.js
@@ -18,6 +18,18 @@ module.exports = {
         ]
       }
     },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
+      }
+    },
     // Edit this step with your custom install commands
     {
       method: "shell.run",
@@ -29,18 +41,6 @@ module.exports = {
           "uv pip install -r requirements.txt",
           "uv pip install -e ."
         ]
-      }
-    },
-    // Delete this step if your project does not use torch
-    {
-      method: "script.start",
-      params: {
-        uri: "torch.js",
-        params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
-        }
       }
     },
 //    {
