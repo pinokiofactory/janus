fd776054fd010399d7f263c03fc76538642b53eb
cocktailpeanut <cocktailpeanuts@proton.me>
2025-01-27 13:58:01 -0500

    update

--- torch.js
+++ torch.js
@@ -7,7 +7,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.0.1 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu121 --force-reinstall"
+        "message": "uv pip install torch<=2.0.1 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu121 --force-reinstall"
       }
     },
     // windows amd
@@ -27,7 +27,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.0.1 numpy==1.26.4 --force-reinstall"
+        "message": "uv pip install torch<=2.0.1 numpy==1.26.4 --force-reinstall"
       }
     },
     // mac
@@ -37,7 +37,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.0.1 --force-reinstall"
+        "message": "uv pip install torch<=2.0.1 --force-reinstall"
       }
     },
     // linux nvidia
@@ -47,7 +47,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.0.1 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu121 --force-reinstall"
+        "message": "uv pip install torch<=2.0.1 {{args && args.xformers ? 'xformers' : ''}} --index-url https://download.pytorch.org/whl/cu121 --force-reinstall"
       }
     },
     // linux rocm (amd)
@@ -57,7 +57,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.0.1 --index-url https://download.pytorch.org/whl/rocm6.0 --force-reinstall"
+        "message": "uv pip install torch<=2.0.1 --index-url https://download.pytorch.org/whl/rocm6.0 --force-reinstall"
       }
     },
     // linux cpu
@@ -67,7 +67,7 @@ module.exports = {
       "params": {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
-        "message": "uv pip install torch==2.0.1 --index-url https://download.pytorch.org/whl/cpu --force-reinstall"
+        "message": "uv pip install torch<=2.0.1 --index-url https://download.pytorch.org/whl/cpu --force-reinstall"
       }
     }
   ]
